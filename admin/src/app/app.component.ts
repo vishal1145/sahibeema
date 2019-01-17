dc6395f1982ebb058521a39ae0aca6af0e16de04
vishal <vishal.kumar1145@gmail.com>
2019-01-17 15:50:34 +0530

    commit changes

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,25 +1,50 @@
-import { Component } from '@angular/core';
-import { ITHoursService } from '../providers/it-hours-service';
+import { Component, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+
+import { RoutePartsService } from "./shared/services/route-parts.service";
+import { ThemeService } from './shared/services/theme.service';
+
+import { filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ITHoursService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'admin';
-  constructor (
-   public itHoursService:ITHoursService
-  ){
-   
+export class AppComponent implements OnInit, AfterViewInit {
+  appTitle = 'Egret';
+  pageTitle = '';
+
+  constructor(
+    public title: Title, 
+    private router: Router, 
+    private activeRoute: ActivatedRoute,
+    private routePartsService: RoutePartsService,
+    private themeService: ThemeService,
+    private renderer: Renderer2
+  ) { }
+
+  ngOnInit() {
+    this.changePageTitle();
   }
 
-  async getProducts(){
-   var input = {
-    "modelName":"Category"
+  ngAfterViewInit() {
+    this.themeService.applyMatTheme(this.renderer)
   }
-    let res = await this.itHoursService.executeByGet(input, false);
-    console.log(res)
+  changePageTitle() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((routeChange) => {
+      var routeParts = this.routePartsService.generateRouteParts(this.activeRoute.snapshot);
+      if (!routeParts.length)
+        return this.title.setTitle(this.appTitle);
+      // Extract title from parts;
+      this.pageTitle = routeParts
+                      .reverse()
+                      .map((part) => part.title )
+                      .reduce((partA, partI) => {return `${partA} > ${partI}`});
+      this.pageTitle += ` | ${this.appTitle}`;
+      this.title.setTitle(this.pageTitle);
+    });
   }
-
+  
 }
