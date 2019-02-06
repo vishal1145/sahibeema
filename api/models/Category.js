47a49fa8e076a566f4a5c86c840af29c5feb14c6
vishal1145 <vishal.kumar1145@gmail.com>
2019-02-06 12:11:25 +0530

    push

--- api/models/Category.js
+++ api/models/Category.js
@@ -5,6 +5,9 @@ var CategorySchema = new mongoose.Schema({
     title: String,
     image : String,
     icon : String,
+	important_facts: String,
+	insurance_type: String,
+	biggest_ins_company: String,
     created_at: { type: Date, default: Date.now }
 });
 
